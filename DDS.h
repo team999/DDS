e81414c9d391099db7e63b203b680e5cb3685bd7
Andre Geldenhuis <none@none>
2012-02-13 13:53:07 +1300

    Corrected some licensing stuff

--- DDS.h
+++ DDS.h
@@ -6,8 +6,8 @@
 //		Andre Geldenhuis (andre.geldenhuis@gmail.com)
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -15,7 +15,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
+// You should have received a copy of the GNU Lesser Public License
 // along with This program.  If not, see <http://www.gnu.org/licenses/>.
 // 
 // DDS components based on:
